7d2affb57c5bacdbd671183f319fe525e87aa49a
寫詩羊 <Habuche@gmail.com>
2019-01-22 18:21:21 +0800

    把上面的表單獨立成一個 component 並改使用 sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import Form from './components/Form';
+import './App.scss';
 
 class App extends Component {
 	constructor(props) {
@@ -49,7 +50,7 @@ class App extends Component {
 						<p className="card-text">{this.getExcludeTax.price}</p>
 						<h6 className="card-title">稅金</h6>
 						<p className="card-text">{this.getExcludeTax.taxValue}</p>
-						<div>{this.getExcludeTax.price} + {this.getExcludeTax.taxValue} =  {this.getExcludeTax.price + this.getExcludeTax.taxValue}</div>
+						<div>{this.getExcludeTax.price} + {this.getExcludeTax.taxValue} = {this.getExcludeTax.price + this.getExcludeTax.taxValue}</div>
 					</div>
 				</div>
 			</div>
@@ -73,32 +74,11 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="jumbotron jumbotron-fluid">
-					<div className="container">
-						<div className="text-right">
-							<a className="github-button" href="https://github.com/winwu/react-business-tax/issues" aria-label="Issue winwu/react-business-tax on GitHub">Issue</a>
-						</div>
-						<h1 className="display-4">營業稅試算</h1>
-						<p className="lead">練習 React 用，算錯不負責 XD</p>
-						<div className="form-row">
-							<div className="col-auto mb-3">
-								<label htmlFor="price-input">金額</label>
-								<input id="price-input" className="form-control" type="text" value={this.state.price} pattern="[0-9.]+" onChange={this.handlePriceChange}/>
-							</div>
-							<div className="col-auto">
-								<div className="mb-3">
-									<label htmlFor="tax-input">設定稅額</label>
-									<div className="input-group">
-										<input id="tax-input" className="form-control" type="text" value={this.state.tax} pattern="[0-9.]+" onChange={this.handleTaxChange}/>
-										<div className="input-group-append">
-											<div className="input-group-text">%</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+        <Form
+          {...this.state}
+          handleTaxChange={ this.handleTaxChange }
+          handlePriceChange={ this.handlePriceChange }
+        />
 				<div className="container mt-3 mb-3">
 					{ this.renderWarning() }
 					{ isNaN(this.state.price) === false && isNaN(this.state.tax) === false  && this.renderResult() ? this.renderResult() : null }
